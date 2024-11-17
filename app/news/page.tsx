adc0121d503100bb29542bfae64d09e46a85b799
Robinho0413 <101249228+Robinho0413@users.noreply.github.com>
2024-11-17 16:30:16 +0100

    Refactor page components to enhance styling and layout consistency

--- app/news/page.tsx
+++ app/news/page.tsx
@@ -1,3 +1,10 @@
 export default function Page() {
-    return <h1>Actualités</h1>
+    return (
+        <div className="p-16">
+            <h1 className="text-3xl font-semibold mb-8 relative inline-block">
+                Actualités
+                <span className="absolute left-0 bottom-0 w-0 h-0.5 bg-primary-500 animate-underline"></span>
+            </h1>
+        </div>
+    );
 }
--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,12 +1,14 @@
-import PricingTable from "@/components/sections/pricingTable";
+import PricingTable from '@/components/sections/pricingTable';
+import React from 'react';
 
 export default function Page() {
     return (
         <div className="p-16">
-            <h1 className="text-3xl font-semibold">Inscription</h1>
-            <div className="w-full">
-                <PricingTable />
-            </div>
+            <h1 className="text-3xl font-semibold mb-8 relative inline-block">
+                Inscriptions
+                <span className="absolute left-0 bottom-0 w-0 h-0.5 bg-primary-500 animate-underline"></span>
+            </h1>
+            <PricingTable />
         </div>
-    )
+    );
 }
